d24833c1bb30740619b552d0f6570f5034373473
Prajwal14 <prazwalgoel96@gmail.com>
2022-05-02 21:06:47 +0530

    Update s3.tf

--- tf-code/s3.tf
+++ tf-code/s3.tf
@@ -1,27 +1,27 @@
-resource "aws_s3_bucket" "bucket1" {
-  bucket = "bucket1"
+# resource "aws_s3_bucket" "bucket1" {
+#   bucket = "bucket1"
 
-  tags = {
-    Name        = "My bucket 1"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "My bucket 1"
+#     Environment = "Dev"
+#   }
+# }
 
-resource "aws_s3_bucket" "bucket2" {
-  bucket = "bucket2"
+# resource "aws_s3_bucket" "bucket2" {
+#   bucket = "bucket2"
 
-  tags = {
-    Name        = "My bucket 2"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "My bucket 2"
+#     Environment = "Dev"
+#   }
+# }
 
-resource "aws_s3_bucket_acl" "bucket1" {
-  bucket = aws_s3_bucket.bucket1.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_acl" "bucket1" {
+#   bucket = aws_s3_bucket.bucket1.id
+#   acl    = "private"
+# }
 
-resource "aws_s3_bucket_acl" "bucket2" {
-  bucket = aws_s3_bucket.bucket2.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_acl" "bucket2" {
+#   bucket = aws_s3_bucket.bucket2.id
+#   acl    = "private"
+# }
