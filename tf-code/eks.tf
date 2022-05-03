b331194e7e87719970657a1ceb4c326cad6916ce
Prajwal14 <prazwalgoel96@gmail.com>
2022-05-03 19:19:08 +0530

    Update eks.tf

--- tf-code/eks.tf
+++ tf-code/eks.tf
@@ -1,14 +1,14 @@
-# module "eksCluster" {
-#   source          = "../modules/EKS"
+module "eksCluster" {
+  source          = "../modules/EKS"
 
-#   project_naming  = local.naming
+  project_naming  = local.naming
 
-#   subnet_ids    = module.eks_vpc.private_subnet_id
-#   endpoint_public_access  = true
-#   endpoint_private_access = true
+  subnet_ids    = module.eks_vpc.private_subnet_id
+  endpoint_public_access  = true
+  endpoint_private_access = true
 
-#   node_group_desired_size   = 2
-#   node_group_max_size       = 4
-#   node_group_min_size       = 2
-#   node_group_instance_types = ["t3.large"]
-# }
+  node_group_desired_size   = 2
+  node_group_max_size       = 4
+  node_group_min_size       = 2
+  node_group_instance_types = ["t3.large"]
+}
